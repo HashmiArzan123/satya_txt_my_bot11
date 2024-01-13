5992a14fd4e0e4bf5ffe4aa883aaf7718d0dbc26
TIGER <141488144+tiger7815@users.noreply.github.com>
2024-01-13 21:05:14 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,13 +4,13 @@ API_ID = API_ID = 22439323
 
 API_HASH = os.environ.get("API_HASH", "e0e203c8be2c2c58b04d0c59fc82f507")
 
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "6343803228:AAHyn7G51Ip7M75W3uxZodnjU092Xp3AAxg")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "6334241757:AAHo1iSa_LcjP6jbbEO-Wz1H8wp9Oj9tZSk")
 
 PASS_DB = int(os.environ.get("PASS_DB", "721"))
 
 OWNER = int(os.environ.get("OWNER", 6090912349))
 
-LOG = -1001847674314
+LOG = -1002021098463
 
 try:
     ADMINS=[]
